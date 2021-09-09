d0d18f12e993883dce1b66f0c593972a7e387b39
rajyammahesh <90377831+rajyammahesh@users.noreply.github.com>
2021-09-09 13:09:35 +0530

    Update views.py
    add h1 tag

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("<h1>Welcome to Azure - Batch 1 Training</h1>")
